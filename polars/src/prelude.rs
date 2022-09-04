d4b32e7597f706fdea1da745b0df18b29143b141
William Dillon <william.dillon@racepointenergy.com>
2022-09-04 17:13:11 +0000

    Trying out fixing the prelude

--- polars/src/prelude.rs
+++ polars/src/prelude.rs
@@ -8,3 +8,5 @@ pub use polars_lazy::prelude::*;
 pub use polars_ops::prelude::*;
 #[cfg(feature = "temporal")]
 pub use polars_time::prelude::*;
+#[cfg(feature = "polars-algo")]
+pub use polars_algo::prelude::*;
